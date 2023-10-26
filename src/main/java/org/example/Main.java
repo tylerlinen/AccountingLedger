092a0f119fbc3e8e62c59a35153331c634f1d9b4
Tyler <tylerlinen@gmail.com>
2023-10-26 14:20:51 -0400

    updated ledger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -24,13 +23,16 @@ public class Main {
         System.out.println("Enter Amount");
         String a = scanner.nextLine();
 
-        String s = d + "|" + tod + "|" + des + "|" + v + "|" + a + "\n";
 
-        FileWriter fileWriter = new FileWriter("ledger.csv");
-        fileWriter.write(s);
+        String s = d + "|" + tod + "|" + des + "|" + v + "|" + a + "|" + true + "\n";
+
+        FileWriter fileWriter = new FileWriter("ledger.csv", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(s);
+        bufferedWriter.close();
         fileWriter.close();
 
-
+        //file reader append
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -64,12 +66,14 @@ public class Main {
         System.out.println("Enter Amount");
         String a = scanner.nextLine();
 
-        String s = d + "|" + tod + "|" + des + "|" + v + "|" + a + "\n";
 
-        FileWriter fileWriter = new FileWriter("ledger.csv");
-        fileWriter.write(s);
+        String s = d + "|" + tod + "|" + des + "|" + v + "|" + a + "|" + false + "\n";
+
+        FileWriter fileWriter = new FileWriter("ledger.csv", true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(s);
+        bufferedWriter.close();
         fileWriter.close();
-
 
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -91,7 +95,7 @@ public class Main {
         }
     }
 
-    public static void ledgerScreen(){
+    public static void ledgerScreen() throws IOException {
         System.out.println("Ledger");
         System.out.println("A) Display All");
         System.out.println("D) Display All Deposits");
@@ -99,31 +103,96 @@ public class Main {
         System.out.println("R) Reports");
         System.out.println("H) Home");
 
-        String l = scanner.nextLine();
-        String input = null;
+        FileReader fileReader = new FileReader("ledger.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        switch (l) {
-            case "A":
-                System.out.println("hello");
-                break;
-            case "D":
-                System.out.println("hi");
-                break;
-            case "P":
-                System.out.println("displayed negative entries");
-                break;
-            case "R":
-                System.out.println("displayed reports");
-                break;
-            case "H":
-                System.out.println("displayed home");
-                break;
+        String user = null;
+
+
+        while ((user = bufferedReader.readLine()) != null) {
+            String[] userData = user.toString().split("\\|");
+
+
+            LocalDate date = LocalDate.parse(userData[0]);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            LocalDateTime time = LocalDateTime.parse(userData[1], formatter);
+            String description = userData[2];
+            String vendor = userData[3];
+            double amount = Double.parseDouble(userData[4]);
+            boolean deposit = Boolean.parseBoolean(userData[5]);
+
+            Transaction t = new Transaction(date, time, description, vendor, amount, deposit);
+
+            String l = scanner.nextLine();
+
+            if (l == "A"){
+                System.out.println(deposit);
+            }
+
         }
-    }
 
+
+
+
+//            switch (l) {
+//                case "A":
+//                    System.out.println(user);
+//                    break;
+//                case "D":
+//                    if (deposit == true) {
+//                        System.out.println(t);
+//                    } else {
+//                        System.out.println("");
+//                    }
+//                    break;
+//                case "P":
+//                    if (deposit == false) {
+//                        System.out.println(t);
+//                    } else {
+//                        System.out.println("");
+//                    }
+//                    break;
+//                case "R":
+//                    System.out.println("1) Month to Date");
+//                    System.out.println("2) Previous Date");
+//                    System.out.println("3) Year to Date");
+//                    System.out.println("4) Previous Year");
+//                    System.out.println("5) Search by Vendor");
+//                    System.out.println("6) Back");
+//
+//                    String p = scanner.nextLine();
+//
+//                    switch (p) {
+//                        case "1":
+//                            System.out.println(1);
+//                            break;
+//                        case "2":
+//                            System.out.println(2);
+//                            break;
+//                        case "3":
+//                            System.out.println(3);
+//                            break;
+//                        case "4":
+//                            System.out.println(4);
+//                            break;
+//                        case "5":
+//                            System.out.println(5);
+//                            break;
+//                        case "6":
+//                            System.out.println(6);
+//                            break;
+//                    }
+//
+//                    break;
+//                case "H":
+//                    System.out.println(2);
+//                    break;
+//
+//        }
+        bufferedReader.close();
+    }
     public static void main(String[] args) {
         try {
-
             //Home Screen
             System.out.println("Home Screen");
             System.out.println("D)" + "Add Deposit");
@@ -153,32 +222,6 @@ public class Main {
                 Exception e) {
             e.printStackTrace();
         }
-//
-
-//        System.out.println("D)" + "Add Deposit");
-//        System.out.println("P)" + "Make Payment (Debit)");
-//        System.out.println("L)" + "Ledger");
-//        System.out.println("X)" + "Exit");
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        String choice = scanner.nextLine();
-//
-//        switch (choice){
-//            case "D":
-//                System.out.println("D)" + "Add Deposit");
-//                break;
-//            case "P":
-//                System.out.println("P)" + "Make Payment (Debit)");
-//                break;
-//            case "L":
-//                System.out.println("L)" + "Ledger");
-//                break;
-//            case "X":
-//                System.out.println("X)" + "Exit");
-//                break;
-//        }
-
 
     }
 }
