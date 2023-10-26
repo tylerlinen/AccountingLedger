87adfd63cb898624b1d3c235314819d228948fa0
Tyler <tylerlinen@gmail.com>
2023-10-26 19:37:13 -0400

    functioning..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,15 @@ import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.Year;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = null;
-
+    //Displays home screen
     public static void homeScreen() throws IOException {
         //Home Screen
-        System.out.println("Home Screen");
+        System.out.println("\nHome Screen");
         System.out.println("D)" + "Add Deposit");
         System.out.println("P)" + "Make Payment (Debit)");
         System.out.println("L)" + "Ledger");
@@ -34,11 +32,12 @@ public class Main {
                 ledgerScreen();
                 break;
             case "X":
-                System.out.println("X)" + "Exit");
+                System.exit(0);
                 break;
         }
     }
 
+    //Adding a deposit transaction
     public static void makeDeposit() throws IOException {
         //Deposit
         Scanner scanner = new Scanner(System.in);
@@ -83,6 +82,7 @@ public class Main {
         }
     }
 
+    //Adding a payment transaction
     public static void makePayment() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter date in format YYYY-MM-DD");
@@ -125,9 +125,10 @@ public class Main {
         }
     }
 
+    //Displays ledger screen
     public static void ledgerScreen() throws IOException {
 
-        System.out.println("Ledger");
+        System.out.println("\nLedger");
         System.out.println("A) Display All");
         System.out.println("D) Display All Deposits");
         System.out.println("P) Display Negative Entries");
@@ -156,6 +157,7 @@ public class Main {
         }
     }
 
+    //Finds and displays all transactions
     public static void displayAll() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -182,6 +184,7 @@ public class Main {
         }
     }
 
+    //Finds and displays all deposit transactions
     public static void displayDeposits() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -210,6 +213,7 @@ public class Main {
         }
     }
 
+    //Finds and displays all payment transactions
     public static void displayPayments() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -236,6 +240,8 @@ public class Main {
             }
         }
     }
+
+    //Find and displays all transactions this month
     public static void monthToDate() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -262,12 +268,14 @@ public class Main {
 
             Month m = date.getMonth();
 
-            if (month.equals(m)){
+            if (month.equals(m)) {
                 System.out.println(t);
             }
 
         }
     }
+
+    //Find and displays all transactions from last month
     public static void previousMonth() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -293,12 +301,14 @@ public class Main {
             Month month = currentDate.getMonth();
             Month m = date.getMonth();
 
-            if (month.equals(m)){
+            if (month.equals(m)) {
                 System.out.println(t);
             }
 
         }
     }
+
+    //Find and displays all transactions this year
     public static void yearToDate() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -324,12 +334,14 @@ public class Main {
             int year = currentDate.getYear();
             int y = date.getYear();
 
-            if (year == y){
+            if (year == y) {
                 System.out.println(t);
             }
 
         }
     }
+
+    //Find and displays all transactions last year
     public static void previousYear() throws IOException {
         FileReader fileReader = new FileReader("ledger.csv");
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -355,49 +367,85 @@ public class Main {
             int year = currentDate.getYear();
             int y = date.getYear();
 
-            if (year == y){
+            if (year == y) {
                 System.out.println(t);
             }
 
         }
     }
 
-    public static void displayReports() throws IOException {
-        System.out.println("1) Month To Date");
-        System.out.println("2) Previous Month");
-        System.out.println("3) Year To Date");
-        System.out.println("4) Previous Year");
-        System.out.println("5) Search By Vendor");
-        System.out.println("6) Back");
+    //Searches and displays all transactions by vendor
+    public static void searchVendor() throws IOException {
+        FileReader fileReader = new FileReader("ledger.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
+        String user = null;
 
-        switch (value){
-            case "1":
-                monthToDate();
-                break;
-            case "2":
-               previousMonth();
-                break;
-            case "3":
-                yearToDate();
-                break;
-            case "4":
-                previousYear();
-                break;
-            case "5":
-                monthToDate();
-                break;
-            case "6":
-                monthToDate();
-                break;
+
+        while ((user = bufferedReader.readLine()) != null) {
+
+            String[] userData = user.toString().split("\\|");
+
+            LocalDate date = LocalDate.parse(userData[0]);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            LocalDateTime time = LocalDateTime.parse(userData[1], formatter);
+            String description = userData[2];
+            String vendor = userData[3];
+            double amount = Double.parseDouble(userData[4]);
+            boolean deposit = Boolean.parseBoolean(userData[5]);
+
+            Transaction t = new Transaction(date, time, description, vendor, amount, deposit);
+
+
         }
 
     }
+
+    //Displays reports screen
+    public static void displayReports() throws IOException {
+        boolean done = false;
+
+        do {
+            System.out.println("\n1) Month To Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search By Vendor");
+            System.out.println("6) Back");
+
+
+            scanner = new Scanner(System.in);
+            String value = scanner.nextLine();
+
+            switch (value) {
+                case "1":
+                    monthToDate();
+                    break;
+                case "2":
+                    previousMonth();
+                    break;
+                case "3":
+                    yearToDate();
+                    break;
+                case "4":
+                    previousYear();
+                    break;
+                case "5":
+                    searchVendor();
+                    break;
+                case "6":
+                    ledgerScreen();
+                    break;
+            }
+        } while (!done);
+    }
+
     public static void main(String[] args) {
+        boolean done = false;
         try {
-            homeScreen();
+            do {
+                homeScreen();
+            } while (!done);
         } catch (
                 Exception e) {
             e.printStackTrace();
